cff7bf73921a515212b7b3a3fc06d7d2d27f58ca
clayton <crdcastro5@gmail.com>
2023-05-17 15:50:42 -0700

    add: timezone to date formatting in slack messages

--- cmd/slack.go
+++ cmd/slack.go
@@ -67,7 +67,7 @@ func slacknotif(release Release, owner string, repo string, channel string) erro
 			},
 			{
 				"type": "mrkdwn",
-				"text": "Authored by: ` + release.Author.Login + ` on ` + publishedDate.Format("Jan 2, 2006") + ` at ` + publishedDate.Format("3:4pm") + ` UTC"
+				"text": "Authored by: ` + release.Author.Login + ` on ` + publishedDate.Format("Jan 2, 2006") + ` at ` + publishedDate.In(time.UTC).Format("3:4pm MST") + `"
 			}
 			]
 		}
