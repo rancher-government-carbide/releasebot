de7e84ae3086358ea7278b6d64910302f0fdc753
clayton <crdcastro5@gmail.com>
2023-07-11 20:12:27 -0700

    update: better logging for config_test

--- cmd/config_test.go
+++ cmd/config_test.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"testing"
 )
 
 const configfile string = "../config.json"
 
+// checks if the example configuration can be parsed properly, relies on predetermined contents of the config.json file
 func Test_loadConfig(t *testing.T) {
 
 	os.Setenv("releasebot_config", configfile)
@@ -22,39 +22,44 @@ func Test_loadConfig(t *testing.T) {
 	var testcase int = 0
 	var passed int = 0
 	var failed int = 0
+	var entry string
+	const passed_testcase string = "Passed %s testcase"
+	const failed_testcase string = "Failed %s testcase"
 
+	entry = "clanktron/dummy"
 	if config[testcase].Owner == "clanktron" && config[testcase].Repo == "dummy" && config[testcase].Prereleases && config[testcase].Tekton {
-		// t.Logf("Passed Test: %d ", testcase+1)
+		t.Logf(passed_testcase, entry)
 		passed++
 	} else {
-		fmt.Println(config[testcase])
+		t.Logf(failed_testcase, entry)
 		failed++
 	}
 	testcase++
 
+	entry = "rancher/rancher"
 	if config[testcase].Owner == "rancher" && config[testcase].Repo == "rancher" {
-		// t.Logf("Passed Test")
+		t.Logf(passed_testcase, entry)
 		passed++
 	} else {
-		// t.Logf("Failed Test")
+		t.Logf(failed_testcase, entry)
 		failed++
 	}
 	testcase++
 
 	if config[testcase].Owner == "k3s-io" && config[testcase].Repo == "k3s" {
-		// t.Logf("Passed Test")
+		t.Logf(passed_testcase, entry)
 		passed++
 	} else {
-		// t.Logf("Failed Test")
+		t.Logf(failed_testcase, entry)
 		failed++
 	}
 	testcase++
 
 	if config[testcase].Owner == "kubernetes" && config[testcase].Repo == "kubernetes" {
-		// t.Logf("Passed Test")
+		t.Logf(passed_testcase, entry)
 		passed++
 	} else {
-		// t.Logf("Failed Test")
+		t.Logf(failed_testcase, entry)
 		failed++
 	}
 	testcase++
