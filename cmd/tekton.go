fec3bd2260b1cd8bde072e8824503e3fcf2e4a43
clayton <crdcastro5@gmail.com>
2023-07-20 12:47:20 -0700

    hotfix: switch back to old tekton payload

--- cmd/tekton.go
+++ cmd/tekton.go
@@ -14,9 +14,14 @@ var tekton_namespace = os.Getenv("tekton_namespace")
 var tekton_listener = os.Getenv("tekton_listener")
 var tekton_port = os.Getenv("tekton_port")
 
-type publish_chart_trigger struct {
-	Helm_repo   string `json:"helm_repo"`
-	Release_tag string `json:"release_tag"`
+// type publish_chart_trigger struct {
+// 	Helm_repo   string `json:"helm_repo"`
+// 	Release_tag string `json:"release_tag"`
+// }
+
+type standard_trigger struct {
+	Product string `json:"product"`
+	Release string `json:"release"`
 }
 
 func triggertekton(release Release, owner string, repo string) error {
@@ -32,12 +37,17 @@ func triggertekton(release Release, owner string, repo string) error {
 	}
 
 	var tektonurl string = fmt.Sprintf("http://el-%s-listener.%s.svc.cluster.local:%s", tekton_listener, tekton_namespace, tekton_port)
-	var repo_url string = fmt.Sprintf("git@github.com:%s/%s", owner, repo)
+	// var repo_url string = fmt.Sprintf("git@github.com:%s/%s", owner, repo)
 
-	trigger := publish_chart_trigger{
-		Helm_repo:   repo_url,
-		Release_tag: release.TagName,
+	trigger := standard_trigger{
+		Product: repo,
+		Release: release.TagName,
 	}
+
+	// chart_trigger := publish_chart_trigger{
+	// 	Helm_repo:   repo_url,
+	// 	Release_tag: release.TagName,
+	// }
 
 	json_trigger, err := json.Marshal(trigger)
 	if err != nil {
@@ -63,3 +73,28 @@ func triggertekton(release Release, owner string, repo string) error {
 
 	return nil
 }
+
+// func deliver_payload(payload interface{}) error {
+//
+// 	json_trigger, err := json.Marshal(trigger)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	req, err := http.NewRequest("POST", tektonurl, bytes.NewBuffer(json_trigger))
+// 	if err != nil {
+// 		return err
+// 	}
+// 	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+// 	client := &http.Client{}
+// 	resp, err := client.Do(req)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer resp.Body.Close()
+// 	body, err := io.ReadAll(resp.Body)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	log.Println("tekton response Status:", resp.Status)
+// 	log.Println("tekton response Body:", string(body))
+// }
