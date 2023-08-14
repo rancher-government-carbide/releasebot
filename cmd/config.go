f8e1b3360a49f52f0897c56e3cb6d01bac049e4c
clayton <crdcastro5@gmail.com>
2023-08-13 23:56:38 -0700

    add: support for loading the payload config file, relevant tests for such

--- cmd/config.go
+++ cmd/config.go
@@ -6,20 +6,65 @@ import (
 	"os"
 )
 
-type Repository struct {
-	Owner       string `json:"owner"`
-	Repo        string `json:"repo"`
-	Prereleases bool   `json:"prereleases"`
-	Tekton      bool   `json:"tekton"`
-	Slack       bool   `json:"slack"`
+type RepositoryEntry struct {
+	Owner       string     `json:"owner"`
+	Repo        string     `json:"repo"`
+	Prereleases bool       `json:"prereleases"`
+	Payloads    PayloadMap `json:"payloads"`
+	Slack       bool       `json:"slack"`
 }
 
-func loadConfig(config *[]Repository) error {
+type PayloadMap map[string]bool
 
-	configpath := os.Getenv("releasebot_config")
+type PayloadEntry struct {
+	Name    string          `json:"name"`
+	Url     string          `json:"url"`
+	Payload json.RawMessage `json:"payload"`
+}
+
+func (p *PayloadMap) UnmarshalJSON(data []byte) error {
+	var payloadStrings []string
+	if err := json.Unmarshal(data, &payloadStrings); err != nil {
+		return err
+	}
+
+	payloadMap := make(map[string]bool)
+	for _, payload := range payloadStrings {
+		payloadMap[payload] = true
+	}
+
+	*p = payloadMap
+	return nil
+}
+
+func loadRepos(config *[]RepositoryEntry) error {
+
+	configpath := os.Getenv("RELEASEBOT_REPOS")
 	if configpath == "" {
-		log.Printf("Defaulting to ./config.json since no config path was specified.\n")
-		configpath = "config.json"
+		log.Printf("Defaulting to ./repos.json since no config path was specified.\n")
+		configpath = "repos.json"
+	}
+
+	configFile, err := os.Open(configpath)
+	if err != nil {
+		return err
+	}
+	defer configFile.Close()
+
+	err = json.NewDecoder(configFile).Decode(config)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func loadPayloads(config *[]PayloadEntry) error {
+
+	configpath := os.Getenv("RELEASEBOT_PAYLOADS")
+	if configpath == "" {
+		log.Printf("Defaulting to ./payloads.json since no config path was specified.\n")
+		configpath = "payloads.json"
 	}
 
 	configFile, err := os.Open(configpath)
--- cmd/config_test.go
+++ cmd/config_test.go
@@ -1,74 +1,215 @@
 package main
 
 import (
+	"encoding/json"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
-const configfile string = "../config.json"
+type testMetrics struct {
+	testcaseCount   int
+	passedTestcases int
+	failedTestcases int
+}
+
+const repoFile string = "../repos.json"
+const payloadFile string = "../payloads.json"
+
+func processResult(t *testing.T, entryField string, passed bool, metrics *testMetrics) {
+	if passed {
+		metrics.passedTestcases++
+	} else {
+		t.Errorf("Failed: Incorrect %s field", entryField)
+		metrics.failedTestcases++
+	}
+	metrics.testcaseCount++
+
+}
 
 // checks if the example configuration can be parsed properly, relies on predetermined contents of the config.json file
-func Test_loadConfig(t *testing.T) {
+func Test_loadRepos(t *testing.T) {
 
-	os.Setenv("releasebot_config", configfile)
+	var metrics = testMetrics{
+		testcaseCount:   0,
+		passedTestcases: 0,
+		failedTestcases: 0,
+	}
+	os.Setenv("RELEASEBOT_REPOS", repoFile)
 
-	var config []Repository
-	if err := loadConfig(&config); err != nil {
-		// t.Logf("Error loading config file...\nExiting...\n")
+	var testRepos []RepositoryEntry
+	if err := loadRepos(&testRepos); err != nil {
+		t.Fatalf("Error loading repo file: %v", err)
+	}
+
+	repoIndex := 0
+	if testRepos[repoIndex].Owner == "clanktron" {
+		processResult(t, "owner", true, &metrics)
 	} else {
-		t.Logf("Loaded config file successfully...\n")
+		processResult(t, "owner", false, &metrics)
 	}
-
-	var testcase int = 0
-	var passed int = 0
-	var failed int = 0
-	var entry string
-	const passed_testcase string = "Passed %s testcase"
-	const failed_testcase string = "Failed %s testcase"
-
-	entry = "clanktron/dummy"
-	if config[testcase].Owner == "clanktron" && config[testcase].Repo == "dummy" && config[testcase].Prereleases && config[testcase].Tekton {
-		t.Logf(passed_testcase, entry)
-		passed++
+	if testRepos[repoIndex].Repo == "dummy" {
+		processResult(t, "repo", true, &metrics)
 	} else {
-		t.Logf(failed_testcase, entry)
-		failed++
+		processResult(t, "repo", false, &metrics)
 	}
-	testcase++
-
-	entry = "rancher/rancher"
-	if config[testcase].Owner == "rancher" && config[testcase].Repo == "rancher" {
-		t.Logf(passed_testcase, entry)
-		passed++
+	if testRepos[repoIndex].Slack == true {
+		processResult(t, "slack", true, &metrics)
 	} else {
-		t.Logf(failed_testcase, entry)
-		failed++
+		processResult(t, "slack", false, &metrics)
 	}
-	testcase++
-
-	if config[testcase].Owner == "k3s-io" && config[testcase].Repo == "k3s" {
-		t.Logf(passed_testcase, entry)
-		passed++
+	if testRepos[repoIndex].Prereleases == true {
+		processResult(t, "prereleases", true, &metrics)
 	} else {
-		t.Logf(failed_testcase, entry)
-		failed++
+		processResult(t, "prereleases", false, &metrics)
 	}
-	testcase++
+	payloads := []string{"standard", "test"}
+	for _, payload := range payloads {
+		if !testRepos[repoIndex].Payloads[payload] {
+			processResult(t, "payloads", false, &metrics)
+		}
+	}
+	repoIndex++
 
-	if config[testcase].Owner == "kubernetes" && config[testcase].Repo == "kubernetes" {
-		t.Logf(passed_testcase, entry)
-		passed++
+	if testRepos[repoIndex].Owner == "rancher" {
+		processResult(t, "owner", true, &metrics)
 	} else {
-		t.Logf(failed_testcase, entry)
-		failed++
+		processResult(t, "owner", false, &metrics)
 	}
-	testcase++
+	if testRepos[repoIndex].Repo == "rancher" {
+		processResult(t, "repo", true, &metrics)
+	} else {
+		processResult(t, "repo", false, &metrics)
+	}
+	if testRepos[repoIndex].Slack == true {
+		processResult(t, "slack", true, &metrics)
+	} else {
+		processResult(t, "slack", false, &metrics)
+	}
+	if testRepos[repoIndex].Prereleases == true {
+		processResult(t, "prereleases", true, &metrics)
+	} else {
+		processResult(t, "prereleases", false, &metrics)
+	}
+	payloads = []string{"standard", "test"}
+	for _, payload := range payloads {
+		if !testRepos[repoIndex].Payloads[payload] {
+			processResult(t, "payloads", false, &metrics)
+		}
+	}
+	repoIndex++
 
-	if passed > 0 {
-		t.Logf("Passed %d/%d testcases", passed, testcase)
+	if testRepos[repoIndex].Owner == "k3s-io" {
+		processResult(t, "owner", true, &metrics)
+	} else {
+		processResult(t, "owner", false, &metrics)
 	}
-	if failed > 0 {
-		t.Errorf("Failed %d/%d testcases", failed, testcase)
+	if testRepos[repoIndex].Repo == "k3s" {
+		processResult(t, "repo", true, &metrics)
+	} else {
+		processResult(t, "repo", false, &metrics)
+	}
+	if testRepos[repoIndex].Slack == true {
+		processResult(t, "slack", true, &metrics)
+	} else {
+		processResult(t, "slack", false, &metrics)
+	}
+	if testRepos[repoIndex].Prereleases == true {
+		processResult(t, "prereleases", true, &metrics)
+	} else {
+		processResult(t, "prereleases", false, &metrics)
+	}
+	payloads = []string{"standard"}
+	for _, payload := range payloads {
+		if !testRepos[repoIndex].Payloads[payload] {
+			processResult(t, "payloads", false, &metrics)
+		}
+	}
+	repoIndex++
+
+	if testRepos[repoIndex].Owner == "kubernetes" {
+		processResult(t, "owner", true, &metrics)
+	} else {
+		processResult(t, "owner", false, &metrics)
+	}
+	if testRepos[repoIndex].Repo == "kubernetes" {
+		processResult(t, "repo", true, &metrics)
+	} else {
+		processResult(t, "repo", false, &metrics)
+	}
+	if testRepos[repoIndex].Slack == true {
+		processResult(t, "slack", true, &metrics)
+	} else {
+		processResult(t, "slack", false, &metrics)
+	}
+	repoIndex++
+
+	if metrics.failedTestcases > 0 {
+		t.Errorf("Failed %d/%d testcases", metrics.failedTestcases, metrics.testcaseCount)
+	}
+
+}
+
+func Test_loadPayloads(t *testing.T) {
+
+	correctPayloads := []PayloadEntry{
+		{
+			Name: "standard",
+			Url:  "https://el-example-listener.tekton.svc.cluster.local:8080",
+			Payload: json.RawMessage(`
+				{
+					"Product": "$REPO",
+            		"Release": "$RELEASE.TAGNAME"
+				}
+        `),
+		},
+		{
+			Name: "helm-chart",
+			Url:  "https://el-example1-listener.tekton.svc.cluster.local:8080",
+			Payload: json.RawMessage(`
+				{
+					"helm_repo": "$REPO_URL",
+            		"release_tag": "$RELEASE.TAGNAME"
+				}
+
+			`),
+		},
+		{
+			Name: "example",
+			Url:  "https://el-example2-listener.tekton.svc.cluster.local:8080",
+			Payload: json.RawMessage(`
+				{
+					"Other-stuff": "$RELEASE.TAGNAME",
+            		"Something-else": "$RELEASE.PUBLISHEDAT",
+            		"More_stuff": "AUTHOR.LOGIN"
+				}
+
+			`),
+		},
+	}
+
+	os.Setenv("RELEASEBOT_PAYLOADS", payloadFile)
+
+	var testPayloads []PayloadEntry
+	if err := loadPayloads(&testPayloads); err != nil {
+		t.Fatalf("Error loading payloads file: %v", err)
+	}
+
+	if len(correctPayloads) != len(testPayloads) {
+		t.Errorf("Payload arrays have different lengths")
+	}
+
+	for i := 0; i < len(correctPayloads) && i < len(testPayloads); i++ {
+		if correctPayloads[i].Name != testPayloads[i].Name {
+			t.Errorf("Payload name mismatch at index %d: expected %s, got %s", i, correctPayloads[i].Name, testPayloads[i].Name)
+		}
+
+		if correctPayloads[i].Url != testPayloads[i].Url {
+			t.Errorf("Payload URL mismatch at index %d: expected %s, got %s", i, correctPayloads[i].Url, testPayloads[i].Url)
+		}
+
+		require.JSONEq(t, string(correctPayloads[i].Payload), string(testPayloads[i].Payload))
 	}
 
 }
